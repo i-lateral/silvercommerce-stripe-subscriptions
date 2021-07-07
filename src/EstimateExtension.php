9d8345d828553a37018c464642d7d4092fa2026a
Morven Lewis-Everley <morven@i-lateral.com>
2021-07-07 18:43:17 +0100

    Get array of data for Stripe from an estimate

--- src/EstimateExtension.php
+++ src/EstimateExtension.php
@@ -3,6 +3,8 @@
 namespace ilateral\SilverCommerce\StripeSubscriptions;
 
 use SilverStripe\ORM\DataExtension;
+use SilverCommerce\ContactAdmin\Model\Contact;
+use SilverCommerce\OrdersAdmin\Model\Estimate;
 
 /**
  * Add Stripe data to an estimate
@@ -13,4 +15,37 @@ class EstimateExtension extends DataExtension
         'StripeSubscriptionID' => 'Varchar',
         'StripeSessionID' => 'Varchar'
     ];
+
+    public function getStripeData(): array
+    {
+        /** @var Estimate */
+        $owner = $this->getOwner();
+
+        // retrieve customer direct from DB, so we get updated data
+        $customer = Contact::get()->byID($owner->CustomerID);
+
+        $product = $owner->Items()->first()->FindStockItem();
+
+        $data = [
+            'items' => [],
+            'payment_behavior' => 'default_incomplete',
+            'expand' => ['latest_invoice.payment_intent'],
+        ];
+
+        foreach ($owner->Items() as $line_item) {
+            $product = $line_item->FindStockItem();
+
+            if (!isset($product) || empty($product->StripeID)) {
+                continue;
+            }
+
+            $data['items'][] = ['price' => $product->StripeID];
+        }
+
+        if (isset($customer) && !empty($customer->StripeID)) {
+            $data['customer'] = $customer->StripeID;
+        }
+
+        return $data;
+    }
 }
